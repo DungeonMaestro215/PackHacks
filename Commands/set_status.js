c7c957e1edf68655fd7c997bec7488b063dbe315
Nathaniel Badgett <badnat@live.unc.edu>
2021-03-20 15:08:51 -0400

    added basic set status command

---
+++ Commands/set_status.js
@@ -0,0 +1,14 @@
+// set status
+// 
+module.exports = {
+    name: 'set status',
+    alias: ['set Status', 'Set status', 'Set Status'],
+    description: 'Sets your status',
+    args: true,
+    usage: '<status name>',
+    cooldown: 5,
+    guildOnly: false,
+    execute(msg, args) {
+        msg.channel.send('Your status is now set to ' + args[0]);
+    }
+}
