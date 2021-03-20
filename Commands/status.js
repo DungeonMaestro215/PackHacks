e3bf4a14bbd572590fcede4161633796345aeaa4
Denver Massey <denmas22@live.unc.edu>
2021-03-20 17:19:10 -0400

    Changed Commands to lowercase c

--- Commands/status.js
+++ Commands/status.js
@@ -12,9 +12,11 @@ module.exports = {
     cooldown: 1,
     guildOnly: false,
     execute(msg, args) {
+        // Read user file
         let users = fs.readFileSync(filename);
         users = JSON.parse(users);
 
+        // Find the user referred to
         try {
             let id = msg.channel.members.find(user => user.user.username == args[0] || user.nickname == args[0]).id;
             if (users[id]) {
