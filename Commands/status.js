7011e0dceacb4ed86f6de7603fcb6ab969006bff
Denver Massey <denmas22@live.unc.edu>
2021-03-20 16:59:24 -0400

    Status implemented

--- Commands/status.js
+++ Commands/status.js
@@ -9,16 +9,19 @@ module.exports = {
     description: 'Displays the status of a specefied user',
     args: false,
     usage: '<user>',
-    cooldown: 5,
+    cooldown: 1,
     guildOnly: false,
     execute(msg, args) {
-        let users = {};
-        fs.readFile(filename, (err, data) => {
-            if (err) throw err;
-            users = JSON.parse(data);
-        });
-        if(users[msg.channel.client.users.get('name', args[0]).id]) {
-            msg.channel.send(users[msg.author]["status"]);
+        let users = fs.readFileSync(filename);
+        users = JSON.parse(users);
+
+        try {
+            let id = msg.channel.client.users.cache.find(user => user.username == args[0]).id;
+            if (users[id]) {
+                msg.channel.send(users[id]["status"]);
+            }
+        } catch (e) {
+            msg.channel.send("There is no status for that user. Check your spelling.");
         }
     }
 }
