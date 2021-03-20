da904df27c6f9a452c8419877a28b7a2fc7a5fd0
Denver Massey <denmas22@live.unc.edu>
2021-03-20 16:04:48 -0400

    Set_status now updates the json file

--- Commands/set_status.js
+++ Commands/set_status.js
@@ -1,7 +1,10 @@
 // set status
 // 
 
-const users = require('../users.json');
+// const users = require('../users.json');
+const fs = require('fs');
+const users = {};
+const filename = "./users.json";
 
 module.exports = {
     name: 'set_status',
@@ -12,12 +15,30 @@ module.exports = {
     cooldown: 5,
     guildOnly: false,
     execute(msg, args) {
-        msg.channel.send('Your status is now set to ' + args[0]);
+        // If the user doesn't have a spot, add one
         if (!users[msg.author]) {
             users[msg.author] = {};
         }
-        users[msg.author]["status"][args.contains("dnd")] = args[0];
 
-        console.log(users);
+        users[msg.author]["dnd"] = args.includes("dnd");
+
+        args.filter(element => element !== "dnd");
+
+        users[msg.author]["status"] = args[0];
+
+        let data = JSON.stringify(users, null, 2);
+
+        fs.writeFile(filename, data, (err) => {
+            if (err) throw err;
+            console.log("Data written to file");
+        });
+
+        fs.readFile(filename, (err, data) => {
+            if (err) throw err;
+            let users = JSON.parse(data);
+            console.log(users);
+        });
+
+        // msg.author.setActivity(args[0], { type: "PLAYING" });
     }
 }
